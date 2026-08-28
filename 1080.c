1b99cae7480aea08719685b0eea90e0f2d7cff48
victormacedo724 <victor08120118@gmail.com>
2026-08-28 11:59:39 -0300

    Add header comments to 1080.c
    Added header comments with problem details and author information.

--- 1080.c
+++ 1080.c
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algoritimo e Estrutura de Dados 2026S1
+Nome        : Victor Hugo Ribeiro de Macedo
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 28/08/2026
+Objetivo    : Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+Dificuldade : facil (nivel 2)
+Uso de IA   : usei so pra fazer a varredura do codigo, a unica que mudei foi o (j+1)
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 100
